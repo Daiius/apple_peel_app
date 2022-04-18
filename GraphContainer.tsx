418f3e9869744b803a6e3cdb1ff306cb22bdabe8
Daiji Yamashita <yamashita.daiji@gmail.com>
2022-04-18 22:10:05 +0900

    use javascript plotly function to plot graph

--- GraphContainer.tsx
+++ GraphContainer.tsx
@@ -1,7 +1,7 @@
 
 interface State {
-  x: [number];
-  y: [number];
+  x: number[];
+  y: number[];
 };
 
 interface Props {
@@ -13,30 +13,38 @@ export default class GraphContainer extends React.Component<Props, State> {
     super(props);
     const x = this.range(-10, 10);
     const a = 1;
-    this.state = {
-      a: a, 
+    this.state = { 
       x: x, 
       y: this.calcY(x, a)
       };
   }
 
-  range = (start, end) => {
-    return Array(end - start + step).fill().map((_,idx) => start + idx)
+  range = (start: number, end: number):number[] => {
+    return Array.from({ length: end - start + 1 }, (v, i) => i);
   }
 
-  calcY = (x: [number], a: number) => {
+  calcY = (x: number[], a: number):number[] => {
     return x.map(xx => xx*xx*a);
   }
 
   onCoeffChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({y: calcY(this.state.x, e.target.value)});
+    this.setState({y: this.calcY(this.state.x, parseInt(e.target.value))});
+    Plotly.newPlot("graph",[{
+      x: this.state.x, y: this.state.y, type: "scatter"
+    }]);
+  }
+
+  componentDidMount() {
+    Plotly.newPlot("graph",[{
+      x: this.state.x, y: this.state.y, type: "scatter"
+    }]);
   }
 
   render() {
     return (
       <div>
-        <PlotlyChart data={data}/>
-        <input type="range" min={1} max={10} step={0.1}/>
+        <div id="graph"></div>
+        <input type="range" min={1} max={10} step={0.1} onChange={this.onCoeffChange}/>
       </div>
     );
   }
